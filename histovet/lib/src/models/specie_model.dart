0863262c26e484e2c2133c0d15881c0228899300
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-04-05 00:09:21 -0500

    mejorar clase modelo especie

--- histovet/lib/src/models/specie_model.dart
+++ histovet/lib/src/models/specie_model.dart
@@ -1,29 +1,10 @@
 import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-List<SpecieModel> speciesFromJson(dynamic str) =>
-    List<SpecieModel>.from((str).map((x) => SpecieModel.fromJson(x)));
+class Specie {
+  final String? id;
+  final String code;
+  final String name;
 
-class SpecieModel {
-  late String? id;
-  late String? code;
-  late String? name;
-
-  SpecieModel({this.id, this.code, this.name});
-
-  SpecieModel.fromJson(Map<String, dynamic> json) {
-    id = json["_id"];
-    code = json["code"];
-    name = json["name"];
-  }
-
-  Map<String, dynamic> toJson() {
-    final _data = <String, dynamic>{};
-
-    _data["_id"] = id;
-    _data["code"] = code;
-    _data["name"] = name;
-
-    return _data;
-  }
+  Specie(this.id, this.code, this.name);
 }

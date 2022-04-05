68ba84361834315134dd3ff4a1344ba196d42f2a
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-04-05 00:09:34 -0500

    agregar especies a la BD

---
+++ histovet/lib/src/pages/add_species.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:histovet/src/models/specie_model.dart';
+import 'package:histovet/src/pages/widgets/widget_drawer.dart';
+
+import '../models/breed_model.dart';
+
+class AddSpecie extends StatefulWidget {
+  static String id = "AddSpecie_page";
+  final Function(Specie) insertSpecie;
+
+  AddSpecie(this.insertSpecie);
+
+  @override
+  State<AddSpecie> createState() => _AddSpecieState();
+}
+
+class _AddSpecieState extends State<AddSpecie> {
+  TextStyle txtStyle = TextStyle(fontSize: 26, fontWeight: FontWeight.bold);
+  TextEditingController codeController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+
+  buildTextFormField(hint, iconData, textController, inputType) {
+    return Container(
+        margin: EdgeInsets.symmetric(vertical: 8),
+        child: TextFormField(
+          keyboardType: inputType,
+          controller: textController,
+          decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: hint,
+              prefixIcon: Icon(iconData)),
+        ));
+  }
+
+  void AddSpecie() {
+    final Specie breed;
+    breed = Specie("", codeController.text, nameController.text);
+    widget.insertSpecie(breed);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Text("Agregar Especie", style: txtStyle),
+            buildTextFormField(
+                "Code", Icons.code, codeController, TextInputType.text),
+            buildTextFormField(
+                "Name", Icons.text_fields, nameController, TextInputType.text),
+            Container(
+              margin: EdgeInsets.all(8),
+              width: double.infinity,
+              child: ElevatedButton(
+                child: Text(
+                  "Agregar Especie",
+                  style: txtStyle.copyWith(
+                      fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+                onPressed: () {
+                  AddSpecie();
+                },
+              ),
+            )
+          ],
+        ));
+  }
+}
--- histovet/lib/src/pages/species_page.dart
+++ histovet/lib/src/pages/species_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:histovet/src/models/specie_model.dart';
 import 'package:histovet/src/pages/add_breed.dart';
+import 'package:histovet/src/pages/add_species.dart';
 import 'package:histovet/src/pages/widgets/widget_drawer.dart';
+import 'package:histovet/src/services/FirebaseService.dart';
 import "package:path/path.dart" as Path;
 
 class SpeciesPage extends StatefulWidget {
@@ -12,13 +15,22 @@ class SpeciesPage extends StatefulWidget {
 }
 
 class _SpeciesPageState extends State<SpeciesPage> {
+  final FirebaseService _service = FirebaseService();
+
+  void addSpecieInDB(Specie specie) {
+    _service.storeSpecieToFirebase(specie).whenComplete(() {
+      Navigator.of(context).pop();
+    });
+  }
+
   void showAddSpecieModal() {
-    /*showModalBottomSheet(
+    showModalBottomSheet(
         context: context,
         builder: (context) {
           return Padding(
-              padding: MediaQuery.of(context).viewInsets, child: AddSpecie());
-        }); */
+              padding: MediaQuery.of(context).viewInsets,
+              child: AddSpecie(addSpecieInDB));
+        });
   }
 
   @override

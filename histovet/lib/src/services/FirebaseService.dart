b8aea730fd72c5b20d8b98b95ef899efb69fdf99
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-04-05 00:09:59 -0500

    Agregar Especie a la BD

--- histovet/lib/src/services/FirebaseService.dart
+++ histovet/lib/src/services/FirebaseService.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../models/breed_model.dart';
+import '../models/specie_model.dart';
 
 class FirebaseService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
@@ -10,5 +11,12 @@ class FirebaseService {
 
     await breedDoc
         .set({"id": breedDoc.id, "code": breed.code, "name": breed.name});
+  }
+
+  Future<void> storeSpecieToFirebase(Specie specie) async {
+    final DocumentReference specieDoc = _firestore.collection("specie").doc();
+
+    await specieDoc
+        .set({"id": specieDoc.id, "code": specie.code, "name": specie.name});
   }
 }

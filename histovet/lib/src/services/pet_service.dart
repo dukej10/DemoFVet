96552f3052e028597208bf6ec8ee9d384b71cb1a
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-04-26 20:40:33 -0500

    servicios pet

---
+++ histovet/lib/src/services/pet_service.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../models/pet_model.dart';
+
+import 'package:firebase_database/firebase_database.dart';
+class PetService {
+   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final databaseRef = FirebaseDatabase.instance.ref();
+
+  CollectionReference petAll =
+      FirebaseFirestore.instance.collection("pet");
+
+  Future<void> storePetToFirebase(Pet specie) async {
+    final DocumentReference specieDoc = _firestore.collection("pet").doc();
+
+    if (specie.id!.isNotEmpty) {
+      await FirebaseFirestore.instance.collection("pet").doc(specie.id).set(
+          {"id": specie.id, "code": specie.code, "name": specie.name, "nameOwner": specie.nameOwner, "contactOwner": specie.contactOwner, "age": specie.age, "breed": specie.breed, "specie": specie.specie, "color":specie.color, "sex": specie.sex},
+          SetOptions(merge: true));
+    } else {
+      await specieDoc
+          .set({"id": specieDoc.id, "code": specie.code, "name": specie.name, "nameOwner": specie.nameOwner, "contactOwner": specie.contactOwner, "age": specie.age, "breed": specie.breed, "specie": specie.specie, "color":specie.color, "sex": specie.sex});
+    }
+  }
+
+  Future<void> deletePetFromFirebase(id) async {
+    await _firestore.collection("pet").doc(id).delete();
+  }
+
+  Future<List<Pet>> getPets() async {
+    List<Pet> especies = [];
+    final collection = FirebaseFirestore.instance.collection('pet');
+    collection.snapshots().listen((querySnapshot) {
+      for (var doc in querySnapshot.docs) {
+        Map<String, dynamic> data = doc.data();
+        Pet newPet = Pet(data["id"], data["code"], data["name"], data["nameOwner"], data["contactOwner"],data["age"], data["breed"], data["specie"], data["color"], data["sex"] );
+        especies.add(newPet);
+      }
+    });
+    return especies;
+  }
+
+}

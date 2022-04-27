1c8fe9eb26b0e0ed4653d8cc56fca14c53ce142f
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-04-27 00:06:36 -0500

    cambio store pet

--- histovet/lib/src/services/pet_service.dart
+++ histovet/lib/src/services/pet_service.dart
@@ -10,16 +10,22 @@ class PetService {
   CollectionReference petAll =
       FirebaseFirestore.instance.collection("pet");
 
-  Future<void> storePetToFirebase(Pet specie) async {
-    final DocumentReference specieDoc = _firestore.collection("pet").doc();
+  Future<bool> storePetToFirebase(Pet specie) async {
+    try {
+      final DocumentReference specieDoc = _firestore.collection("pet").doc();
 
     if (specie.id!.isNotEmpty) {
       await FirebaseFirestore.instance.collection("pet").doc(specie.id).set(
           {"id": specie.id, "code": specie.code, "name": specie.name, "nameOwner": specie.nameOwner, "contactOwner": specie.contactOwner, "documentOwner": specie.documentOwner,"age": specie.age, "breed": specie.breed, "specie": specie.specie, "color":specie.color, "sex": specie.sex},
           SetOptions(merge: true));
+       return true;
     } else {
       await specieDoc
           .set({"id": specieDoc.id, "code": specie.code, "name": specie.name, "nameOwner": specie.nameOwner, "contactOwner": specie.contactOwner, "documentOwner": specie.documentOwner, "age": specie.age, "breed": specie.breed, "specie": specie.specie, "color":specie.color, "sex": specie.sex});
+          return true;
+    }
+    } catch (e) {
+      return false;
     }
   }
 
@@ -28,17 +34,22 @@ class PetService {
   }
 
   Future<List<Pet>> getPets() async {
-    List<Pet> especies = [];
+    List<Pet> mascotas = [];
+    try {
     final collection = FirebaseFirestore.instance.collection('pet');
     collection.snapshots().listen((querySnapshot) {
       for (var doc in querySnapshot.docs) {
         Map<String, dynamic> data = doc.data();
-        print(data);
+        print(doc.data());
         Pet newPet = Pet(data["id"], data["code"], data["name"], data["nameOwner"], data["contactOwner"],data["documentOwner"],data["age"], data["breed"], data["specie"], data["color"], data["sex"] );
-        especies.add(newPet);
+        mascotas.add(newPet);
       }
     });
-    return especies;
+    return mascotas;
+    } catch (e) {
+      return mascotas;
+    }
+    
   }
 
 }

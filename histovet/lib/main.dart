85a5cd79d95381935dd33ea4ee2219500e171d51
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-04-27 00:04:59 -0500

    nueva ruta

--- histovet/lib/main.dart
+++ histovet/lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
       initialRoute: PetsPage.id,
       routes: {
         PetsPage.id: (context) => PetsPage(),
+        '/pets': (context) => PetsPage(),
         addPet.id: (context) => addPet()
       },
     );

d7ae5565d3661a04e33e9a3411d1185c4d108ee4
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-04-26 21:27:43 -0500

    documento dueño

--- histovet/lib/src/models/pet_model.dart
+++ histovet/lib/src/models/pet_model.dart
@@ -7,12 +7,13 @@ class Pet {
   final String name;
   final String nameOwner;
   final String contactOwner;
+  final String documentOwner;
   final int age;
   final String breed;
   final String specie;
   final String color;
   final String sex;
 
-  Pet(this.id, this.code, this.name, this.nameOwner, this.contactOwner,
+  Pet(this.id, this.code, this.name, this.nameOwner, this.contactOwner, this.documentOwner,
       this.age, this.breed, this.specie, this.color, this.sex);
 }

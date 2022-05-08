0a9308120899e4d11eb0b367908f4fb97c29c9dc
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-05-07 20:41:16 -0500

    Forma de enlistar mascotas

--- histovet/lib/src/pages/pet/pets_page.dart
+++ histovet/lib/src/pages/pet/pets_page.dart
@@ -16,70 +16,91 @@ class PetsPage extends StatefulWidget {
 }
 
 class _PetsPageState extends State<PetsPage> {
-  TextStyle txtStyle = TextStyle(fontWeight: FontWeight.w600, fontSize: 20);
+  TextStyle txtStyle = TextStyle(fontWeight: FontWeight.w900, fontSize: 30, color: Colors.black);
   PetController petCont = new PetController();
   bool respuesta = false;
 
   @override
   Widget build(BuildContext context) {
-    
     return SafeArea(
       child: Scaffold(
           appBar: AppBar(
             centerTitle: true,
             title: Text("Mascotas"),
-            actions: [IconButton(onPressed: () {setState(() {
-              });
-            }, icon: const Icon(Icons.refresh))],
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    setState(() {});
+                  },
+                  icon: const Icon(Icons.refresh))
+            ],
           ),
           drawer: MenuLateral(),
           floatingActionButton: FloatingActionButton(
               child: Icon(FontAwesomeIcons.plus),
               elevation: 15.0,
               backgroundColor: Colors.blue,
-              onPressed: () {Navigator.pushNamed(context, addPet.id);}),
-              body: FutureBuilder(
-            future: petCont.allPets(),
-            builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-              List species = snapshot.data ?? [];
-              return ListView(
-                children: [
-                  for (Pet specie in species)
-                    Card(
-                      margin: EdgeInsets.all(6),
-                      elevation: 6,
-                      child: ListTile(
-                        onLongPress: () {Navigator.push(context, MaterialPageRoute(builder: (context) => updatePet(specie.id.toString())));},
-                        leading: Icon(FontAwesomeIcons.paw),
-                        title: Text(specie.name, style: txtStyle),
-                        subtitle: Text(
-                          specie.code,
-                          style: txtStyle.copyWith(fontSize: 17),
-                        ),
-                        trailing: IconButton(
-                          icon: Icon(Icons.delete, color: Colors.red),
-                          onPressed: (){
-                            messageDelete(specie.id.toString());
-                                      Navigator.pushNamed(context, '/pets').then((_) => setState(() {}));
-                          },
-                        )
-                      ),
-                    )
-                ],
-              );
-            })),
+              onPressed: () {
+                Navigator.pushNamed(context, addPet.id);
+              }),
+          body: FutureBuilder(
+              future: petCont.allPets(),
+              builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+                List species = snapshot.data ?? [];
+                return ListView(
+                  children: [
+                    for (Pet specie in species)
+                      Card(
+                        margin: EdgeInsets.all(6),
+                        elevation: 6,
+                        child: Container(
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage('assets/img/fondo.jpg'),
+                                    fit: BoxFit.cover,
+                                    ),),
+                            child: ListTile(
+                                onLongPress: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              updatePet(specie.id.toString())));
+                                },
+                                leading: Icon(FontAwesomeIcons.paw, color: Colors.black,),
+                                title: Text(specie.name, style: txtStyle,),
+                                subtitle: Text(
+                                  specie.code,
+                                  style: txtStyle.copyWith(fontSize: 17),
+                                ),
+                                trailing: IconButton(
+                                  icon: Icon(Icons.delete, color: Colors.black),
+                                  onPressed: () {
+                                    messageDelete(specie.id.toString());
+                                    Navigator.pushNamed(context, '/pets')
+                                        .then((_) => setState(() {}));
+                                  },
+                                ))),
+                      )
+                  ],
+                );
+              })),
     );
   }
 
-  void messageDelete(String  idPet) async{
+  void messageDelete(String idPet) async {
     respuesta = await petCont.deletePet(idPet);
     if (respuesta) {
-            Navigator.pushNamed(context, '/pets').then((_) => setState(() {}));
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Se eliminó la mascota"),
-            backgroundColor: Colors.green,));
-          }else{
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("No se pudo eliminar"),
-            backgroundColor: Colors.green,));
-          }
+      Navigator.pushNamed(context, '/pets').then((_) => setState(() {}));
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Se eliminó la mascota"),
+        backgroundColor: Colors.green,
+      ));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("No se pudo eliminar"),
+        backgroundColor: Colors.green,
+      ));
+    }
   }
 }
